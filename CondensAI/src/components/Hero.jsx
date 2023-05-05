b76e991ca19ea27b4f0d7b5abd7fac2f17a44e12
garyzhuo <gary_zhuo@hotmail.com>
2023-05-04 19:40:59 -0500

    Finished Hero section, w/ intro to app and header

--- CondensAI/src/components/Hero.jsx
+++ CondensAI/src/components/Hero.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import logo from '../assets/assets/logo.svg';
+
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <header className='w-full flex justify-center 
+        items-center flex-col'>
+            <nav className='flex justify-between
+            items-center w-full mb-10 pt-3'>
+                <img src={logo} alt='CondesAI_Logo'
+                    className='w-28 object-contain' />
+                <button
+                    type='button'
+                    onClick={() => window.open('https://github.com/garyzhuo')}
+                    className='black_btn'
+                >
+                    GitHub
+                </button>
+            </nav>
+            <h1 className='head_text'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient'>OpenAI GPT -4</span>
+            </h1>
+            <h2 className='desc'>
+
+                Experience effortless reading with CondensAI,
+                an article summarizer that elegantly distills lengthy
+                articles into crisp and coherent summaries.
+            </h2>
+        </header>
+
     )
 }
 
