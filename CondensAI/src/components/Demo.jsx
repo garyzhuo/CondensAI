f3322fb9019eaa9e1981c9a692e9796b97fad03f
garyzhuo <gary_zhuo@hotmail.com>
2023-05-04 21:30:11 -0500

    Added history tabs

--- CondensAI/src/components/Demo.jsx
+++ CondensAI/src/components/Demo.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets/assets';
 import { useLazyGetSummaryQuery } from '../services/article';
 
-const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
 
 const Demo = () => {
@@ -12,8 +11,33 @@ const Demo = () => {
         summary: '',
     });
 
+    const [allArticles, setAllArticles] = useState([]);
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(
+            localStorage.getItem('articles'),
+        )
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage)
+        }
+    }, [])
+
     const handleSubmit = async (e) => {
-        alert("Submitted")
+        e.preventDefault();
+        const { data } = await getSummary
+            ({ articleUrl: article.url });
+
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary }
+            const updatedAllArticles = [newArticle, ...allArticles];
+
+            setArticle(newArticle);
+            setAllArticles(updatedAllArticles)
+            console.log(newArticle)
+
+            localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+        }
     }
 
     return (
@@ -47,18 +71,65 @@ const Demo = () => {
                     <button
                         type='submit'
                         className='submit_btn
-                        peer-focus:border-gray-700
-                        peer-focus-text-gray-700'
+                    peer-focus:border-gray-700
+                    peer-focus:text-gray-700'
                     >
                         ↵
                     </button>
                 </form>
 
                 {/* Add URL or browser history here maybe? */}
+                <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.reverse().map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className='link_card'
+                        >
+                            <div className='copy_btn'
+                                onClick={() =>
+                                    handleCopy(item.url)}>
+                                <img
+                                    src={copied === item.url ? tick : copy}
+                                    alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                                    className='w-[40%] h-[40%] object-contain'
+                                />
+                            </div>
+                            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                                {item.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
-            {/* Add display results here maybe? */}
+            <div className='my-10 max-w-full flex justify-center items-center'>
+                {isFetching ? (
+                    <img src={loader} alt="loader" className='w-20 h-10 object-contain' />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Well that's embarassing... That wasn't supposed to happen...
+                        <br />
+                        <span className='font-satoshi font-normal text-gray-700'>
+                            {error?.data?.error}
+                        </span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                                Article <span className='blue_gradient'>
+                                    Summary
+                                </span>
+                            </h2>
+
+                        </div>
+                    )
+                )}
+
+            </div>
         </section>
     )
 }
 
-export default Demo
+
+export default Demo;
